e2cb1c83916fc5311340595e300f90df2b708653
Phan Duong <PhanDoung@isb.mt>
2023-06-20 20:55:30 +0700

    remove test files

--- vehicle_event.py
+++
@@ -1,9 +0,0 @@
-from api import *
-
-class VehicleEvent(Event):
-    def __init__(self, vehicle_type):
-        super().__init__()
-        self.vehicle_type = vehicle_type
-
-    def get_data(self):
-        return self.vehicle_type
--- vsl_translate_job.py
+++ vsl_translate_job.py
@@ -1,2 +1,17 @@
 from api import *
 from engine import *
+from frame_reader import FrameReader
+from mediapipe_detector import MediapipeDetector
+from landmark_extractor import LandmarkExtractor
+from text_classifier import TextClassifier
+from output_emittor import OutputEmittor
+
+if __name__ == "__main__":
+    job = Job("vsl_translate")
+    data_stream = job.add_source(FrameReader("frame_reader", 1, 9990))
+    data_stream = data_stream.apply_operator(MediapipeDetector("mediapipe_extractor", 3))
+    data_stream = data_stream.apply_operator(LandmarkExtractor("landmark_extractor", 3))
+    data_stream = data_stream.apply_operator(TextClassifier("text_classifier", 3))
+    data_stream.apply_operator(OutputEmittor("output_emittor", 1, 8000))
+    starter = JobStarter(job)
+    starter.start()
